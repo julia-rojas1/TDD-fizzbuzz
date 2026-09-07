b8cd570f8aac4658a6efa15460e53b01f405e349
julia-rojas1 <julia.rojas195060@alumnos.info.unlp.edu.ar>
2026-09-07 17:48:33 +0000

    node: correccion problemas mencionados por el linter

--- node/fizzbuzz.js
+++ node/fizzbuzz.js
@@ -1,9 +1,9 @@
-function fizzbuzz(n) {
-  if (n === 0) return 0;
-  if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz';
-  if (n % 3 === 0) return 'Fizz';
-  if (n % 5 === 0) return 'Buzz';
-  return n;
+function fizzbuzz (n) {
+  if (n === 0) return 0
+  if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz'
+  if (n % 3 === 0) return 'Fizz'
+  if (n % 5 === 0) return 'Buzz'
+  return n
 }
 
 module.exports = fizzbuzz
